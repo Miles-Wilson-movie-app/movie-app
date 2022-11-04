f658f6c40f1d6d0d09b4e9f4dd8847aa072bc603
Miles Raker <milesraker@gmail.com>
2022-11-04 09:03:59 -0700

    Delete function and Disabled bonus complete- MR

--- js/movieProject.js
+++ js/movieProject.js
@@ -13,61 +13,79 @@
 //         img.src = data[0].url
 //     })
 const movieUrl = `https:evening-fortune-cover.glitch.me/movies`
+
 //fetch to our glitch api database
-function fetchMovies(){
-fetch(movieUrl)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        let html = "";
-        for (const datum of data) {
-            let titleString = datum.title.replace(" ", "-");
-            let cardID = `movie${datum.id}`
-            html += `
+function fetchMovies() {
+    fetch(movieUrl)
+        .then(response => response.json())
+        .then(data => {
+            let html = "";
+            for (const datum of data) {
+                let titleString = datum.title.replace(" ", "-");
+                let cardID = `movie${datum.id}`
+                html += `
             <div class="card" id=${cardID}>
                 <h3>${datum.title}</h3>
                 <p>Rating: ${datum.rating}</p>
                 <button class="btn btn-primary edit" data-id="${datum.id}" >Edit</button>
-                <button class="btn btn-primary delete"  id="delete${titleString}" onclick="clickTester()">Delete</button>
+                <button class="btn btn-primary delete"  data-id="${datum.id}" id="delete${titleString}">Delete</button>
             </div>
             `
-        }
-        // format data to html
-        // update inner html of #movieDisplay
-        $(`#movieDisplay`).html(html);
+            }
+            // format data to html
+            // update inner html of #movieDisplay
+            $(`#movieDisplay`).html(html);
 
 
-    })
-    .then(()=> {
-        let editButtons = document.querySelectorAll(".edit");
-        editButtons.forEach((btn) =>{
-            let btnId= btn.getAttribute('data-id');
-            btn.addEventListener("click", editMovieFields);
-        });
-        // attach event listener
-        // create input fields
-        // change edit -> submit
-        // update call CRUD
-        // Fetch to update the shown movies fetchMovies()
-        });
+        })
+        .then(() => {
+            let editButtons = document.querySelectorAll(".edit");
+            editButtons.forEach((btn) => {
+                btn.addEventListener("click", editMovieFields);
+            });
+            // attach event listener
+            // create input fields
+            // change edit -> submit
+            // update call CRUD
+            // Fetch to update the shown movies fetchMovies()
+
+            // Delete Buttons
+            let deleteButtons = document.querySelectorAll(".delete");
+            deleteButtons.forEach((btn) => {
+                btn.addEventListener("click", deleteMovie);
+            });
+
+        })
+        .catch((err) => console.log("fetchMovies error: ", err));
 
 }
+
 fetchMovies()
 
 // --------------- Add New Movie ---------------
 $('#movieSubmit').click((e) => {
     e.preventDefault();
-    console.log(e)
-    let movieName = $('#movieAdd').val()
-    console.log(movieName)
-    let movieRate = $('#movieRating').val()
-    console.log(movieRate)
+    let movieSubmitBtn = $(`#movieSubmit`)
+    movieSubmitBtn.attr('disabled', true);
+
+    let movieAdd = $('#movieAdd')
+    let movieRating = $('#movieRating')
+    let movieName = movieAdd.val();
+    let movieRate = movieRating.val()
+    movieAdd.val('');
+    // document.getElementById(`#movieRating`).reset();
     const movieInfo = {title: movieName, rating: movieRate,}
     const options = {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(movieInfo),};
-    fetch(movieUrl,options)
+    if(movieName === "")
+    {
+        movieSubmitBtn.attr('disabled', false)
+    }else{
+
+    fetch(movieUrl, options)
         .then(fetchMovies)
-        .catch(error => console.log('Error:',error))
-});
+        .then(() => movieSubmitBtn.attr('disabled', false))// remove disabled attribute
+        .catch(error => console.log('Add new Movie Error: ', error))
+}});
 
 // --------------- Edit Button ---------------
 
@@ -77,17 +95,14 @@ $('#movieSubmit').click((e) => {
 // disable all other edit and delete buttons
 // on update.click => create new body object
 
-function clickTester (){
-    console.log("I got clicked");
-}
 
-function editMovieFields(element){
+function editMovieFields(element) {
     // inside <h3> create an input type text with placeholder as movie title
     // select element for movie rating with current rating selected
     // change edit button to submit button
-    console.log(element.target.attributes[1].nodeValue);
+
     let btnId = element.target.attributes[1].nodeValue;
-    let title = $(`#movie${btnId}>h3`).html()
+    let title = $(`#movie${btnId} > h3`).html()
     let movieUpdateUrl = `https:evening-fortune-cover.glitch.me/movies/${btnId}`
     $(`#movie${btnId}`).html(`
             <div class="card" id=${btnId}>
@@ -104,15 +119,27 @@ function editMovieFields(element){
                 </div>
             `)
 
-    $('#update').click((e)=>{
+    $('#update').click((e) => {
+        let updateBtn = $(`#update`)
+        updateBtn.attr('disabled', true);
         let editInfo = $('#editInput')
         let ratingInfo = $('#movieRatingEdit')
-        fetch(movieUpdateUrl,{method: 'PUT', body: JSON.stringify({
+        fetch(movieUpdateUrl, {
+            method: 'PUT', body: JSON.stringify({
                 title: `${editInfo[0].value}`,
                 rating: `${ratingInfo[0].selectedIndex}`,
-                id: `${btnId}`}),headers: {'Content-Type': 'application/json'},
+                id: `${btnId}`
+            }), headers: {'Content-Type': 'application/json'},
         })
             .then(fetchMovies)
-
+            .catch((err) => console.log("Error in movieUpdate: ", err))
     })
+}
+
+function deleteMovie(element) {
+    let btnId = element.target.attributes[1].nodeValue;
+    let movieDeleteUrl = `https:evening-fortune-cover.glitch.me/movies/${btnId}`;
+    fetch(movieDeleteUrl, {method: 'DELETE',})
+        .then(fetchMovies)
+        .catch((err) => console.log("Error in deleteMovie", err));
 }
